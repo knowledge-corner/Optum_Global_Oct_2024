03ef9cf8f6ef8066a02c5a48208afdfc806031a0
knowledge-corner <vaidehi.nair@knowledgecorner.in>
2024-10-16 17:20:52 +0530

    Day - 3

--- dash app/app.py
+++ dash app/app.py
@@ -7,23 +7,48 @@ app = Dash()  # Initial the app
 
 # Build the layout
 app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.origin.unique(), 'USA', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+    html.H1("MPG Dashboard", style={'text-align': 'center', 'color': 'darkslategray'}),
+    html.Label("Select Origin", style={'font-size': '16px', 'margin-left': '20px'}),
+    dcc.Dropdown(id='dropdown-selection',
+                 value = "All",
+                 options = [{"label" : "All", "value" : "All"}] + 
+                 [{"label" : val, "value" : val} for val in df.origin.unique()],
+                 clearable = False,
+                 style={'width': '50%', 'margin-bottom': '20px', 'margin-left': '10px'}),
+    dcc.Graph(id='scatter'),
+    dcc.Graph(id='bar'),
+    dcc.Graph(id='box')
 ]
 
 # Callbacks
 @callback(
-    Output('graph-content', 'figure'),
+    Output('scatter', 'figure'),
+    Output('bar', 'figure'),
+    Output('box', 'figure'),
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.origin==value]
-    fig = px.scatter(dff, x = "horsepower", y = "mpg", color="origin",
+    if value == "All" :
+        dff = df
+    else:
+        dff = df[df.origin==value]
+    fig_scatter = px.scatter(dff, x = "horsepower", y = "mpg", color="cylinders",
            title= "Correlation between MPG Horsepower",
                    labels={"cylinders" : "Cylinders", "mpg" : "MPG"}, 
                    template="simple_white")
-    return fig
+    
+    fig_bar = px.bar(avg_mpg_by_cyl(dff), x = "cylinders", y = "mpg", 
+                   title= "Average MPG by Cylinders",
+                   labels={"cylinders" : "Cylinders", "mpg" : "Average MPG"}, 
+                   template="simple_white", color_discrete_sequence=["teal"]
+                   )
+    
+    fig_box = px.box(dff, y = "mpg", color="cylinders",
+           title= "IQR Distribution for MGP across Cylinders",
+                   labels={"cylinders" : "Cylinders", "mpg" : "MPG"}, 
+                   template="simple_white")
+
+    return fig_scatter, fig_bar, fig_box
 
 if __name__ == '__main__':
     app.run(debug=True)
--- dash app/mpg.py
+++ dash app/mpg.py
@@ -5,6 +5,6 @@ df = df.loc[df.horsepower.str.isdigit()]
 df.horsepower = df.horsepower.astype(int)
 df.origin = df.origin.map({1 : "USA", 2 : "Germany", 3 : "Japan"})
 
-def avg_mpg_by_cyl():
-    result = df.groupby("cylinders")["mpg"].mean().to_frame().round(2).reset_index()
+def avg_mpg_by_cyl(dff):
+    result = dff.groupby("cylinders")["mpg"].mean().to_frame().round(2).reset_index()
     return result
